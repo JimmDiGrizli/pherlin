8f012e3f9b72dde16ea883582e79e200c87c6879
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-11-13 15:14:24 +0400

    Improved RouteProvider

--- app/Providers/RouterProvider.php
+++ app/Providers/RouterProvider.php
@@ -5,7 +5,8 @@ use GetSky\Phalcon\AutoloadServices\Provider;
 use Phalcon\Config;
 use Phalcon\Mvc\Router;
 
-class RouterProvider implements Provider {
+class RouterProvider implements Provider
+{
 
     /**
      * @var Config
@@ -23,11 +24,43 @@ class RouterProvider implements Provider {
     public function getServices()
     {
         $default = $this->options->get('app')->get('def_module');
+        $modules = $this->options->get('modules');
 
-        return function() use ($default) {
+        return function () use ($default, $modules) {
 
-            $router = new Router();
-            $router->setDefaultModule($default);
+            $router = new \Phalcon\Mvc\Router();
+
+            foreach ($modules as $name => $module) {
+
+                if ($default == $name) {
+                    $router->setDefaultModule($default);
+                    continue;
+                }
+
+                $router->add('#^/'.$name.'(|/)$#', array(
+                        'module' => $name,
+                        'controller' => 'index',
+                        'action' => 'index',
+                    ));
+
+                $router->add(
+                    '#^/'.$name.'/([a-zA-Z0-9\_]+)[/]{0,1}$#',
+                    array(
+                        'module' => $name,
+                        'controller' => 1,
+                    )
+                );
+
+                $router->add(
+                    '#^/'.$name.'[/]{0,1}([a-zA-Z0-9\_]+)/([a-zA-Z0-9\_]+)(/.*)*$#',
+                    array(
+                        'module' => $name,
+                        'controller' => 1,
+                        'action' => 2,
+                        'params' => 3,
+                    )
+                );
+            }
 
             return $router;
         };
