d3999f3810f14cd29921b32a1dcc9ec8f981cf72
Alexander.Getmansky[core] <getmansk-y@yandex.ru>
2013-11-17 22:41:45 +0400

    Added MySQL provider

---
+++ src/GetSky/FrontendModule/Providers/MySqlProvider.php
@@ -0,0 +1,42 @@
+<?php
+namespace GetSky\FrontendModule\Providers;
+
+use GetSky\Phalcon\AutoloadServices\Provider;
+use Phalcon\Config;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+
+class MySqlProvider implements Provider
+{
+    /**
+     * @var Config
+     */
+    private $options;
+
+    public function __construct(Config $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return callable
+     */
+    public function getServices()
+    {
+        $option = $this->options->get('mysql');
+
+        return function () use ($option) {
+            $mysql = new Mysql(array(
+                "host" => $option->host,
+                "username" => $option->username,
+                "password" => $option->password,
+                "persistent" => $option->persistent,
+                "dbname" => $option->name,
+                "options" => array(
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                )
+            ));
+
+            return $mysql;
+        };
+    }
+} 
