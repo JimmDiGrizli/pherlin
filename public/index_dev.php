fa978f0b8209f6f547e8d4a7930d30be65d16aae
JimmDiGrizli <getmansk-y@yandex.ru>
2014-10-10 12:05:01 +0400

    Added standard debug bar for develop environment

--- public/index_dev.php
+++ public/index_dev.php
@@ -5,12 +5,16 @@ ini_set("display_startup_errors", 1);
 
 require_once '../vendor/autoload.php';
 
+use DebugBar\StandardDebugBar;
 use GetSky\Phalcon\Bootstrap\Bootstrap;
 use Phalcon\DI\FactoryDefault;
 
 GetSky\Phalcon\Utils\PrettyExceptions::listenError();
 (new \Phalcon\Debug())->listen();
+$debug = (new StandardDebugBar())->getJavascriptRenderer();
+$debug->setBaseUrl('//rawgit.com/maximebf/php-debugbar/master/src/DebugBar/Resources/');
 
 $app = new Bootstrap(new FactoryDefault());
 $app->setCacheable(false);
-echo $app->run();
+
+echo $app->run().$debug->renderHead().$debug->render();
